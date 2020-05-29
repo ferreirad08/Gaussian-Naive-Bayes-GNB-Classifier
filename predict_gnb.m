7e1ecf3bee7aea08d752855f12798e4721ce7b69
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-05-29 16:16:24 -0400

    Update predict_gnb.m

--- predict_gnb.m
+++ predict_gnb.m
@@ -29,19 +29,20 @@ function label = predict_gnb(X,Y,Xnew)
 %         'virginica'
 
 [C,~,Y] = unique(Y,'stable');
-class_list = 1:length(C);
+binranges = 1:numel(C);
 
 P = size(Xnew,1);
 label = zeros(P,1);
 for inst = 1:P
     % Class prior probability
-    probability = histc(Y,class_list)/length(Y);
-    for j = class_list
+    probability = histc(Y,binranges)/numel(Y);
+    for j = binranges
         A = X(Y==j,:);
         S = std(A);
         
         % Probability density function (PDF) of the normal distribution
-        gauss = 1./(S.*sqrt(2.*pi)).*exp(-1/2.*((Xnew(inst,:)-mean(A))./S).^2);
+        gauss = 1./(S.*sqrt(2.*pi))...
+            .*exp(-1/2.*((Xnew(inst,:)-mean(A))./S).^2);
         
         % Product
         probability(j) = probability(j)*prod(gauss);
